17a27be059272b2e53ca0157f1f9f01ff7a104f0
Jamie Burchfield <burchfield.camilla@gmail.com>
2023-01-29 21:02:25 -0500

    first attempt

---
+++ W4D1/W4D1.rb
@@ -0,0 +1,22 @@
+#will revisit after finishing studying!
+
+class GraphNode
+    attr_accessor :val, :neighbors
+
+    def initialize(val)
+        self.val = val
+        self.neighbors = []
+    end
+
+    def add_neighbor(node)
+        self.neighbors << node
+      end
+    end
+    
+    def bfs(starting_node, target_value)
+
+    end
+
+
+
+end
