629e03f71f646514f9614cf99f9be8b98bfcd4d3
Jamie Burchfield <burchfield.camilla@gmail.com>
2023-01-31 19:56:21 -0500

    Finished specs, need to check for playability

--- W4D3/skeleton/lib/simon.rb
+++ W4D3/skeleton/lib/simon.rb
@@ -10,23 +10,39 @@ class Simon
   end
 
   def play
-
+    take_turn until @game_over == true
+    if @game_over == true
+      game_over_message
+      reset_game
+    end
   end
 
   def take_turn
+    show_sequence
+    require_sequence
 
+    unless @game_over == true
+      round_success_message
+      @sequence_length += 1
+    end
   end
 
   def show_sequence
-
+    add_random_color
   end
 
   def require_sequence
-
+    # The specs don't test require_sequence. The goal of
+    # require_sequence is to
+    #   prompt the user to repeat back the new sequence that was 
+    #   just shown to them, one color at a time. If they give an incorrect 
+    #   color, it should immediately set @game_over to be true.
   end
 
   def add_random_color
-
+    idx = rand(0...COLORS.length)
+    color = COLORS[idx]
+    @seq << color
   end
 
   def round_success_message
@@ -38,6 +54,8 @@ class Simon
   end
 
   def reset_game
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 end
