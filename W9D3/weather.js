2f73d273771fb9902a91f2ddf4fda1c2fb3a9352
Jamie Burchfield <burchfield.camilla@gmail.com>
2023-03-07 20:38:05 -0500

    Simple AJAX Example

---
+++ W9D3/weather.js
@@ -0,0 +1,19 @@
+const id = "bcb83c4b54aee8418983c2aff3073b3b";
+const city = "London,UK";
+
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${id}`)
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error(response);
+    }
+  })
+  .then(data => {
+    console.log("Success!");
+    console.log(data);
+  })
+  .catch(errorResponse => {
+    console.log("Failure!");
+    console.error(errorResponse);
+  });
